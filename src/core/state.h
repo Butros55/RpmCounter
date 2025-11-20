bee472f42dac205e8604660b87c63db1e960957c
Geret Wessling <118867487+Butros55@users.noreply.github.com>
2025-11-20 22:10:05 +0100

    Add Bluetooth UI and async connection handling

--- src/core/state.h
+++ src/core/state.h
@@ -4,6 +4,13 @@
 #include <Arduino.h>
 #include <BLEClient.h>
 #include <BLERemoteCharacteristic.h>
+#include <vector>
+
+struct BleDeviceInfo
+{
+    String name;
+    String address;
+};
 
 extern BLEClient *g_client;
 extern BLERemoteCharacteristic *g_charWrite;
@@ -38,6 +45,24 @@ extern unsigned long g_lastObdMs;
 extern unsigned long g_lastLogoMs;
 extern bool g_engineStartLogoShown;
 extern bool g_ignitionLogoShown;
+
+extern String g_currentTargetAddr;
+extern String g_currentTargetName;
+extern bool g_manualConnectRequested;
+extern int g_manualConnectAttempts;
+extern bool g_manualConnectActive;
+extern bool g_manualConnectFailed;
+extern unsigned long g_manualConnectStartMs;
+extern bool g_connectTaskRunning;
+extern bool g_connectTaskWasManual;
+extern bool g_connectTaskResult;
+extern unsigned long g_connectTaskFinishedMs;
+extern int g_autoReconnectAttempts;
+
+extern bool g_bleScanRunning;
+extern unsigned long g_bleScanStartMs;
+extern unsigned long g_bleScanFinishedMs;
+extern std::vector<BleDeviceInfo> g_bleScanResults;
 
 extern bool g_animationActive;
 extern bool g_logoPlayedThisCycle;
