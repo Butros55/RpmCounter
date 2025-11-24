f01001912db0f1eaf358fb1b8b2fa6af18c08559
Geret Wessling <118867487+Butros55@users.noreply.github.com>
2025-11-24 13:04:17 +0100

    Add ESP32-S3 LVGL UI scaffolding

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,11 @@
 #include "web/web_ui.h"
 #include "hardware/led_bar.h"
 #include "hardware/logo_anim.h"
+#if defined(CONFIG_IDF_TARGET_ESP32S3)
+#include "hardware/display_s3.h"
+#else
 #include "hardware/display.h"
+#endif
 
 #ifndef UNIT_TEST
 void setup()
@@ -18,11 +22,18 @@ void setup()
     setupWifiFromConfig(cfg);
     initWebUi();
     initBle();
+#if defined(CONFIG_IDF_TARGET_ESP32S3)
+    display_s3_init();
+#else
     displayInit();
+#endif
 }
 
 void loop()
 {
+#if defined(CONFIG_IDF_TARGET_ESP32S3)
+    display_s3_loop();
+#endif
     webUiLoop();
     wifiLoop();
     bleObdLoop();
